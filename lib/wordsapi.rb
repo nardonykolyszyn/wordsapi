b73e25e53faed43495613c6abd9b6ffdfb4f4791
Nardo Nykolyszyn <nardonykolyszyn@gmail.com>
2019-01-02 05:44:13 -0500

    add has parts endpoint

--- lib/wordsapi.rb
+++ lib/wordsapi.rb
@@ -38,6 +38,16 @@ class WordsAPI < Base
     OpenStruct.new(success?: false, message: 'Words API is not available')
   end
 
+  def has_parts?(word)
+    response = connection.get has_parts_endpoint(word)
+    response = process_response(response)
+    OpenStruct.new(success?: true, body: response)
+  rescue Faraday::ClientError => exception
+    OpenStruct.new(success?: false, message: 'Wrong data provided', details: exception.response[:body].to_s)
+  rescue Faraday::Error::TimeoutError, Faraday::ConnectionFailed, Timeout::Error => e
+    OpenStruct.new(success?: false, message: 'Words API is not available')
+  end
+
   private
 
   def has_types_endpoint(word)
@@ -50,5 +60,9 @@ class WordsAPI < Base
 
   def part_of_endpoint(word)
     "/#{word}/partOf"
+  end
+
+  def has_parts_endpoint(word)
+    "/#{word}/hasParts"
   end
 end
